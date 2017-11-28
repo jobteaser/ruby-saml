abac7842bbbaec19a03e41fd0ff7a6733adad370
Yann VERY <yann.very@gmail.com>
2017-11-28 18:27:57 +0100

    Add is_required attributes used by RequestedAttribute

--- lib/onelogin/ruby-saml/metadata.rb
+++ lib/onelogin/ruby-saml/metadata.rb
@@ -72,7 +72,8 @@ module OneLogin
             sp_req_attr = sp_acs.add_element "md:RequestedAttribute", {
               "NameFormat" => attribute[:name_format],
               "Name" => attribute[:name],
-              "FriendlyName" => attribute[:friendly_name]
+              "FriendlyName" => attribute[:friendly_name],
+              "isRequired" => attribute[:is_required] || false
             }
             unless attribute[:attribute_value].nil?
               sp_attr_val = sp_req_attr.add_element "md:AttributeValue"
